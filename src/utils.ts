957fba6cbc1ccbdc4a52061703a07f2c01b7f41c
Raveesh Mittal <raveesh.mittal@sabre.com>
2020-11-16 20:30:45 +0530

    Updated market timings for test

--- src/utils.ts
+++ src/utils.ts
@@ -25,7 +25,7 @@ export function isStockMarketOpen(): boolean {
     const currentHour = localMoment.hour();
     const currentMinutes = localMoment.minute();
 
-    if (currentDay >= 1 && currentDay <= 5 && currentHour >= 9 && currentHour <= 17) {
+    if (currentDay >= 1 && currentDay <= 5 && currentHour >= 9 && currentHour <= 22) {
         return true;
     }
     return false;
