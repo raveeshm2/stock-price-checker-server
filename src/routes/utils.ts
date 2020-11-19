3090a3ef92ebbc7041d565719cfee6db5c70b979
Raveesh Mittal <raveesh.mittal@sabre.com>
2020-11-19 21:57:23 +0530

    Updated header

--- src/routes/utils.ts
+++ src/routes/utils.ts
@@ -28,7 +28,7 @@ const commonHeaders = {
     'sec-fetch-dest': 'empty',
     'sec-fetch-mode': 'cors',
     'sec-fetch-site': 'same-origin',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'
 }
 
 let cronGlobal: cron.ScheduledTask | null = null;
